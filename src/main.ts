bbd0e9b0a83ab48ce4144075cea54e613ec04f59
ramirogrisales <ramirogrisales@gmail.com>
2023-09-30 19:35:24 -0500

    fix: change port

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import { CORS } from './constans/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
+  // const configService = app.get(ConfigService)
   app.use(morgan('dev'))
   app.enableCors(CORS)
   app.setGlobalPrefix('api/v1')
-  await app.listen(configService.get('PORT'));
+  // await app.listen(configService.get('PORT'));
+  await app.listen(process.env.PORT || 8080);
   console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
